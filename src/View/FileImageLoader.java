78260f74c93c61032f08d0e3319a12b8c58322e2
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2020-01-14 12:41:32 +0100

    I create the FileImageLoader class to implement the ImageLoader interface.

---
+++ src/View/FileImageLoader.java
@@ -0,0 +1,12 @@
+package View;
+
+import Model.Image;
+
+public class FileImageLoader implements ImageLoader {
+
+    @Override
+    public Image load() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
