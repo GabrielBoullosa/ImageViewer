dc2ead075fe8bc030e0e4effb650f431212bc1f3
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2020-01-14 13:46:04 +0100

    I create the SwingImageDisplay class. This class show the image with Swing into a JPanel.

---
+++ src/View/SwingImageDisplay.java
@@ -0,0 +1,18 @@
+package View;
+
+import Model.Image;
+import javax.swing.JPanel;
+
+public class SwingImageDisplay extends JPanel implements ImageDisplay {
+
+    @Override
+    public Image current() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void show(Image image) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
